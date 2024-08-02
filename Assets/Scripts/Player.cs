f04f46e503eb606c4b108ec39e6fe77578c4d504
강예린 <jjykhs0505@naver.com>
2024-08-03 00:42:45 +0900

    마우스로 움직이기

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,10 +7,24 @@ public class Player : MonoBehaviour
     
     // Update is called once per frame
     void Update()
-    {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
+    { 
+        /*float horizontalInput = Input.GetAxisRaw("Horizontal");
         //float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f);
-        transform.position += moveTo * moveSpeed * Time.deltaTime;
+        Vector3 moveTo1 = new Vector3(horizontalInput, 0f, 0f);
+        transform.position += moveTo1 * moveSpeed * Time.deltaTime;
+
+        Vector3 moveTo2 = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= moveTo2;
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += moveTo2;
+        }*/
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         float toX = Mathf.Clamp(mousePos.x, -2.35f, 2.35f);
+        transform.position = new Vector3(toX, transform.position.y, transform.position.z);
+        
     }
 }
